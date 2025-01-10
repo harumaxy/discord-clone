499334f32c5ed50a686359352f599123ce5167fd
harumaxy <harumaxy@gmail.com>
2025-01-10 14:19:03 +0900

    add chat-welcome component

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,6 @@
 import ChatHeader from "@/components/chat/chat-header";
 import ChatInput from "@/components/chat/chat-input";
+import ChatMessages from "@/components/chat/chat-messages";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
@@ -46,8 +47,20 @@ const ChannelIdPage = async (props: ChannelIdPageProps) => {
         serverId={channel.serverId}
         type="channel"
       />
-      {/* flex col かつ 1 なので、メッセージ部分がheight全体に広がり、Inputが一番下の空いたスペースに入る */}
-      <div className="flex-1">Future Messages</div>
+      <ChatMessages
+        member={member}
+        name={channel.name}
+        chatId={channel.id}
+        type="channel"
+        apiUrl="/api/messages"
+        socketUrl="/api/socket/messages"
+        socketQuery={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+        paramKey="channelId"
+        paramValue={channel.id}
+      />
       <ChatInput
         name={channel.name}
         type="channel"
