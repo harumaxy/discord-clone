2ec4b7af029b7f3caae9263fe8bdf44aeac1dbc0
harumaxy <harumaxy@gmail.com>
2025-01-10 14:19:03 +0900

    add initial modal

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -2,7 +2,7 @@ import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
 
-import CreateServerModal from "@/components/modals/create-server-modal";
+import InitialModal from "@/components/modals/initial-modal";
 
 const SetupPage = async () => {
   const profile = await initialProfile();
@@ -22,7 +22,7 @@ const SetupPage = async () => {
     return redirect(`/servers/${server.id}`);
   }
 
-  return <CreateServerModal />;
+  return <InitialModal />;
 };
 
 export default SetupPage;
