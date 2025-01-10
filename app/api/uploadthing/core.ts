64c24e4a21061fd1bec1122a2264c8095904a8c7
harumaxy <harumaxy@gmail.com>
2025-01-10 14:19:03 +0900

    impl uploadthing endpoint

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,28 @@
+import { auth } from "@clerk/nextjs/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+
+const handleAuth = async () => {
+	const { userId } = await auth();
+	if (!userId) throw new Error("Unauthorized");
+	return { userId };
+};
+
+export const ourFileRouter = {
+	// Discord サーバー画像のアップロードエンドポイント
+	serverImage: f({
+		image: {
+			maxFileSize: "4MB",
+			maxFileCount: 1,
+		},
+	})
+		.middleware(() => handleAuth())
+		.onUploadComplete(() => {}),
+	// Discord メッセージの添付ファイルのアップロードエンドポイント
+	messageFile: f(["image", "pdf"])
+		.middleware(() => handleAuth())
+		.onUploadComplete(() => {}),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
